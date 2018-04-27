916a9a9587bc473f32977002f99431cce457d49e
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2018-04-27 17:51:11 -0500

    add images svg

--- src/app/images.js
+++ src/app/images.js
@@ -1,1 +1,6 @@
-
+import '../images/SVG/magnifying-glass.svg';
+import '../images/SVG/heart.svg';
+import '../images/SVG/twitter.svg';
+import '../images/SVG/github.svg';
+import '../images/SVG/linkedin.svg';
+import '../images/SVG/chevron-down.svg';
